b7dc00b97a118ef112b6c17c84519ac77ea63a7f
ferperezv <frpvillatoro@gmail.com>
2021-04-22 17:10:05 +0300

    Adding functionality to choose genome version

--- R/get_HRDs.R
+++ R/get_HRDs.R
@@ -1,17 +1,24 @@
 #' New HRD scar score
 
-get.ovaHRDscars <- function(seg, chrominfo = chrominfo_grch38, LOH_windos=c(10,50), LST_segSize=12e6, LST_mindistance=1e6){
+get.ovaHRDscars <- function(seg, chrominfo = "grch38", LOH_windos=c(10,50), LST_segSize=12e6, LST_mindistance=1e6){
   seg <- preparing.input(seg)
-
+  
+  if (chrominfo == "grch38"){
+    chrom = chrominfo_grch38
+  }
+  if (chrominfo == "grch37"){
+    chrom = chrominfo_grch37
+  }
+  
   #Calculating HRD-LOH
   HRD_LOHs <- features.LOH(seg, MbSizes=LOH_windos)
   HRD_LOHs <- HRD_LOHs[,1]
 
   #Calculating LSTs
-  LSTs <- LSTs(seg, chrominfo=chrominfo, segsizes=LST_segSize, mindistance=LST_mindistance)
+  LSTs <- LSTs(seg, chrominfo=chrom, segsizes=LST_segSize, mindistance=LST_mindistance)
 
   #Calculating nTAIs
-  res_ai<- calc.ai_new(seg, chrominfo)
+  res_ai<- calc.ai_new(seg, chrom)
   nTAIs <- res_ai[,1]
 
   ovaHRDscar <- HRD_LOHs + LSTs + nTAIs
